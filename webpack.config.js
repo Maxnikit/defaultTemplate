d96ff6f141537c4c58a3122017ee0f7f4af9f195
Maxnikit <max.nikit.03@gmail.com>
2024-02-13 18:26:22 +0300

    add babel and fix gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,16 @@ module.exports = {
 
         type: "asset/resource",
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
     ],
   },
   optimization: {
